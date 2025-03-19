7458dfe01f72a0242fc7a4518cfb6e695480e1fa
MohammedKhalilBerkani <khalilikafo@gmail.com>
2025-03-19 23:53:05 +0100

    Finalisation de exercice 1

--- src/app/home-card/home-card.component.ts
+++ src/app/home-card/home-card.component.ts
@@ -1,24 +1,25 @@
-import { Component, Input } from '@angular/core';
-import {
-  LucideAngularModule,
-  MapPin,
-  WavesLadder,
-  Bed,
-  Bath,
-} from 'lucide-angular';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { LucideAngularModule, WavesLadder, Bed, Bath, MapPin } from "lucide-angular";
+
 import { Home } from '../models/home.type';
 
+/**
+ * Component for displaying a single home card
+ * This is a presentational component that receives a Home object via @Input
+ */
 @Component({
-  selector: 'app-home-card',
-  imports: [LucideAngularModule],
-  templateUrl: './home-card.component.html',
-  styleUrl: './home-card.component.css',
+  selector: "app-home-card",
+  standalone: true,
+  imports: [CommonModule, RouterLink, LucideAngularModule],
+  templateUrl: "./home-card.component.html",
+  styleUrls: ["./home-card.component.css"],
 })
 export class HomeCardComponent {
   @Input() home!: Home;
-  // Icons list
-  readonly MapPin = MapPin;
-  readonly WavesLadderIcon = WavesLadder;
-  readonly BedIcon = Bed;
-  readonly BathIcon = Bath;
+  readonly wavesLadderIcon = WavesLadder;
+  readonly bedIcon = Bed;
+  readonly bathIcon = Bath;
+  readonly mapPinIcon = MapPin;
 }
